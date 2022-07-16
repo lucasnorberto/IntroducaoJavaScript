2553baf09b4cfdb15a35fd2c489de50852938c0c
Lucas Norberto <lucasnorberto@gmail.com>
2022-07-16 00:36:25 -0300

    Update 8-Loops.js

--- 8-Loops.js
+++ 8-Loops.js
@@ -4,7 +4,7 @@ const listaDeDestinos = new Array(`Salvador`, `São Paulo`, `Rio de Janeiro`);
 const idadeComprador = 18;
 const estaAcompanhada = false;
 let temPassagemComprada = false;
-const destino = "São Paulo";
+const destino = "Salvador";
 
 console.log("\n Destinos possíveis:");
 console.log(listaDeDestinos);
@@ -12,12 +12,20 @@ console.log(listaDeDestinos);
 const podeComprar = idadeComprador >= 18 || estaAcompanhada == true;
 
 let contador = 0;
+let destinoExiste = false;
 while (contador < 3) {
+  
   if (listaDeDestinos[contador] == destino){
-    console.log("Destino Existe");
-  } else {
-    console.log("Destino não Existe");
+    destinoExiste = true;
+    break;
   }
-
   contador += 1;
+}
+
+console.log("Destino existe: ", destinoExiste);
+
+if(podeComprar && destinoExiste){
+  console.log("Boa viagem !!");
+}else{
+  console.log("Desculpe tivemos um erro!!");
 }
