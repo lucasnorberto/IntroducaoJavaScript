35deb055020364d1c704e3943373e6cbd3e7f3f5
Lucas Norberto <lucasnorberto@gmail.com>
2022-07-16 00:24:40 -0300

    wil

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -6,21 +6,37 @@ const listaDeDestinos = new Array(
   `Curitiba`
 );
 
-const idadeComprador = 17;
-const estaAcompanhada = true;
+const idadeComprador = 18;
+const estaAcompanhada = false;
+const temPassagemComprada = true;
 
 console.log("Destinos possíveis:");
 console.log(listaDeDestinos);
 
-if (idadeComprador >= 18) {
-  console.log("Comprador maior de idade");
-  listaDeDestinos.splice(1, 1); //removendo item
-} else if (estaAcompanhada) {
-  console.log("Comprador está acompanhado");
-  listaDeDestinos.splice(1, 1); //removendo item
+// if (idadeComprador >= 18) {
+//   console.log("Comprador maior de idade");
+//   listaDeDestinos.splice(1, 1); //removendo item
+// } else if (estaAcompanhada) {
+//   console.log("Comprador está acompanhado");
+//   listaDeDestinos.splice(1, 1); //removendo item
+// } else {
+//   console.log("Não e maior de idade e não posso vender");
+// }
+
+if (idadeComprador >= 18 || estaAcompanhada == true ) {
+  console.log("Boa Viagem !!");
+  listaDeDestinos.splice(2, 1); //removendo item
 } else {
   console.log("Não e maior de idade e não posso vender");
 }
+
+console.log("Embarque: \n\n")
+if(idadeComprador >=18 && temPassagemComprada){
+    console.log("Boa Viagem");
+}else{
+    console.log("Você não pode embarcar"); 
+}
+
 
 console.log(listaDeDestinos);
 
---
+++ 8-Loops.js
@@ -0,0 +1,23 @@
+console.log(`\n Trabalhando com Condicionais`);
+const listaDeDestinos = new Array(`Salvador`, `São Paulo`, `Rio de Janeiro`);
+
+const idadeComprador = 18;
+const estaAcompanhada = false;
+let temPassagemComprada = false;
+const destino = "São Paulo";
+
+console.log("\n Destinos possíveis:");
+console.log(listaDeDestinos);
+
+const podeComprar = idadeComprador >= 18 || estaAcompanhada == true;
+
+let contador = 0;
+while (contador < 3) {
+  if (listaDeDestinos[contador] == destino){
+    console.log("Destino Existe");
+  } else {
+    console.log("Destino não Existe");
+  }
+
+  contador += 1;
+}
