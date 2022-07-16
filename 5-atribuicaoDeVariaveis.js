a3157a3f5fbc8cf1f8d6e443792f8b5699eca54e
Lucas Norberto <lucasnorberto@gmail.com>
2022-07-15 22:32:06 -0300

    Update 5-atribuicaoDeVariaveis.js

--- 5-atribuicaoDeVariaveis.js
+++ 5-atribuicaoDeVariaveis.js
@@ -1,7 +1,7 @@
 
 console.log("Trabalhando com Adribuição de  Variáveis");
 
-const idade = 35;
+
 const primeiroNome = "Lucas";
 const sobrenome = "Norberto";
 
@@ -15,3 +15,16 @@ console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
 const nomeCompelto = primeiroNome + " " + sobrenome
 
 console.log(nomeCompelto);
+
+
+
+let contator = 0;
+contator = contator +1;
+
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
+
+let idade; //declarando variável
+idade = 26; //atribuindo valor
+idade = idade+1;
+console.log(idade)
