f93ce95b386d9f05976e9d468a5695b6c450bd9c
Lucas Norberto <lucasnorberto@gmail.com>
2022-07-15 23:40:01 -0300

    Update 7-condicionais.js

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -1,28 +1,31 @@
 console.log(`Trabalhando com Condicionais`);
 const listaDeDestinos = new Array(
-    `Salvador`,
-    `São Paulo`,
-    `Rio de Janeiro`,
-    `Curitiba`,
-    
+  `Salvador`,
+  `São Paulo`,
+  `Rio de Janeiro`,
+  `Curitiba`
 );
 
 const idadeComprador = 17;
-console.log("Destinos possíveis:");
-console.log (listaDeDestinos);
+const estaAcompanhada = true;
 
-if (idadeComprador >=18){
-    console.log("Comprador maior de idade");
-    listaDeDestinos.splice(1,1); //removendo item
-}else{
-    console.log("Não e maior de idade e não posso vender");
+console.log("Destinos possíveis:");
+console.log(listaDeDestinos);
+
+if (idadeComprador >= 18) {
+  console.log("Comprador maior de idade");
+  listaDeDestinos.splice(1, 1); //removendo item
+} else if (estaAcompanhada) {
+  console.log("Comprador está acompanhado");
+  listaDeDestinos.splice(1, 1); //removendo item
+} else {
+  console.log("Não e maior de idade e não posso vender");
 }
 
 console.log(listaDeDestinos);
 
-console.log(idadeComprador > 18); //Maior que
-console.log(idadeComprador < 18); // Menor que
-console.log(idadeComprador >= 18); // Maior ou Igual a que
-console.log(idadeComprador <= 18); // Menor ou Igua a que
-console.log(idadeComprador == 18); // Igual
-
+// console.log(idadeComprador > 18); //Maior que
+// console.log(idadeComprador < 18); // Menor que
+// console.log(idadeComprador >= 18); // Maior ou Igual a que
+// console.log(idadeComprador <= 18); // Menor ou Igua a que
+// console.log(idadeComprador == 18); // Igual
